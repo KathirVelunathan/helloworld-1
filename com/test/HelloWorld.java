e6e752dc94f9211e9d1013910b367f470039b8f6
prakashkrish <josh.krishprakash@gmail.com>
2017-02-18 00:03:27 +0530

    Delete HelloWorld.java

--- com/test/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World, My first Job tweaked & auto triggered!!");
-   }
-}
