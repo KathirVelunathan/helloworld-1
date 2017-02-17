1259ec9f1e6db7094e2aeba1e9c4c9715a6d6a36
KathirVelunathan <velunathan.lk@gmail.com>
2017-02-17 22:04:31 +0000

    Update StepDefs.java

--- src/test/java/servlet/StepDefs.java
+++ src/test/java/servlet/StepDefs.java
@@ -4,20 +4,16 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import examples.Hello;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 
 public class Stepdefs {
  
-  HttpServletRequest request=new HttpServletRequest();
+ 
 
    @Given("^I have a Get Request for (\\d+)$")
     public void I_have_a_get_request(String l_input) throws Throwable {
         Hello testHello = new Hello();
-        testHello.doGet(request.setParameter("Key",l_input), null);
+        testHello.doGet(null, null);
     }
     
     @When("I submit the request for (\\d+)$")
