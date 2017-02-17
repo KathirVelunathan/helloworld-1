784bbeb8cbc1774a9a474604fc725800e10d2e08
KathirVelunathan <velunathan.lk@gmail.com>
2017-02-17 22:02:13 +0000

    Update StepDefs.java

--- src/test/java/servlet/StepDefs.java
+++ src/test/java/servlet/StepDefs.java
@@ -4,6 +4,10 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import examples.Hello;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 public class Stepdefs {
@@ -13,19 +17,19 @@ public class Stepdefs {
    @Given("^I have a Get Request for (\\d+)$")
     public void I_have_a_get_request(String l_input) throws Throwable {
         Hello testHello = new Hello();
-        TestHello.doGet(request.setParameter("Key",l_input), null);
+        testHello.doGet(request.setParameter("Key",l_input), null);
     }
     
     @When("I submit the request for (\\d+)$")
     public void I_submit_the_request_for(String l_input) throws Throwable {
       Hello testHello = new Hello();
-      TestHello.shouldGenerate(l_input);
+      testHello.shouldGenerate(l_input);
     }
 
       @Then("I should get a response for (\\d+)$")
     public void I_should_get_a_response(String l_input) throws Throwable {
       Hello testHello = new Hello();
-      TestHello.getOutput(l_input);
+      testHello.getOutput(l_input);
     }
 
 }
